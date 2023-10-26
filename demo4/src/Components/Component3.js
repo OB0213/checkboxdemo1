eff9d7d613e327c783843c6f8a37b87ae21aff61
Omkar-Bhosale-912 <omkarbhosale912@gmail.com>
2023-10-26 10:48:21 +0530

    Add Latest Files

---
+++ demo4/src/Components/Component3.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Component3CSS.module.css'
+
+function Component3() {
+  return (
+    <div className={styles.mymain}>
+      <h1 className={styles.mytext}>Hello ABC</h1>
+    </div>
+  )
+}
+
+export default Component3
